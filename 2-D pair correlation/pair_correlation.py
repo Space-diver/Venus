6faa2b6cd9f3f574d2b4086f035b88e98552744c
Space_diver <48621484+Space-diver@users.noreply.github.com>
2019-03-18 08:58:47 +0100

    Update pair_correlation.py

--- 2-D pair correlation/pair_correlation.py
+++ 2-D pair correlation/pair_correlation.py
@@ -1,6 +1,6 @@
 
 # -*- coding: utf-8 -*-
-
+# These codes are adpated from https://github.com/cfinch/Shocksolution_Examples/blob/d050f3a1c6cd7b251dd716dd81139949009f3cee/PairCorrelation/paircorrelation.py
 # python3.6
 
 def pair_correlation(Arr1, Arr2, p, rmax, dr):
